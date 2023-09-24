dc95ae0fc97c4fbee9f63f7e432cf211e9d76d7e
Mohan Krishna Battala <112927860+MohanKrishna-2003@users.noreply.github.com>
2023-09-24 17:25:01 +0530

    Create Solution.java

---
+++ 03. Stacks/04. Previous Greater Element in array /Solution.java
@@ -0,0 +1,53 @@
+// Naive Appraoch
+import java.util.*;
+public class Main 
+{
+    public static void main (String[] args) 
+    {
+        int arr[] = {12, 10, 20, 22, 15, 14, 18, 32, 20, 22, 19};
+        int n = arr.length;
+        for(int i=0; i<n; i++)
+        {
+            boolean flag = false;
+            for(int j=i-1; j>=0; j--)
+            {
+                if(arr[i]<arr[j])
+                {
+                    System.out.print(arr[j] + " ");
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag==false)
+            {
+                System.out.print(" - ");
+            }
+        }
+    }
+}
+
+// Optimized Appraoch
+import java.util.*;
+public class Main 
+{
+    public static void main (String[] args) 
+    {
+        int arr[] = {12, 10, 20, 22, 15, 14, 18, 32, 20, 22, 19};
+        Stack<Integer> s = new Stack<>();
+        s.push(arr[0]);
+        System.out.print(" - ");
+        for(int i=1; i<arr.length; i++)
+        {
+            while(s.isEmpty()==false && s.peek()<=arr[i])
+            {
+                s.pop();
+            }
+            if(s.isEmpty())
+                System.out.print(" - ");
+            else 
+                System.out.print(s.peek() + " ");
+                
+            s.push(arr[i]);
+        }
+    }
+}
